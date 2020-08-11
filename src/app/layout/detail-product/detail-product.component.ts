654380e10381dcb9376c7a9427e48f96e71c13a0
manh3698 <manh3698@gmail.com>
2020-08-11 22:59:59 +0700

    update js slide image detail-product

--- src/app/layout/detail-product/detail-product.component.ts
+++ src/app/layout/detail-product/detail-product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import "jquery"
+declare var jquery:any;
+declare var $ :any;
 
 @Component({
   selector: 'app-detail-product',
@@ -9,8 +10,21 @@ import "jquery"
 export class DetailProductComponent implements OnInit {
 
   constructor() { }
+  
 
   ngOnInit(): void {
+    $(document).ready(function () {
+      $("#owl-similar-post").owlCarousel({
+          loop: true,
+          autoplay: true,
+          navigation: true,
+          slideSpeed: 400,
+          paginationSpeed: 400,
+          items: 1,
+          nav: true,
+          navText: ["<i class='fa fa-angle-left fa-3x'></i>", "<i class='fa fa-angle-right fa-3x'></i>"],
+      });
+  });
   }
 
 }
